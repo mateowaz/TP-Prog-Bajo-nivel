c079933ec1917fc25b249cdc81838bdfc843cf9c
mateowaz <mateowazz@gmail.com>
2025-11-21 15:57:10 -0300

    cambios en el tesrt

--- TestUnitarios.c
+++ TestUnitarios.c
@@ -17,9 +17,9 @@ assert(Lista != NULL);
 Alumno *res = (Alumno *)Lista->dato;
 assert(res->Legajo = a.Legajo);
 assert(strcmp(res->nombre, "Carlos") == 0);
+printf("Test de Agregar Alumno Pasado\n");
 
 LiberarEspacioLista(Lista);
-printf("Test de Agregar Alumno Pasado\n");
 }
 
 void Test_AgregarAlumno_EdadErronea(){
@@ -34,18 +34,50 @@ void *datos = copiar_estudiante(a);
 assert(datos == NULL);
 Lista = agregar(copiar_estudiante(a), tipo_Alumno, Lista);
 assert(Lista != NULL);
-
-LiberarEspacioLista(Lista);
 printf("Test Edad Erronea Pasado\n");
+LiberarEspacioLista(Lista);
 }
 
+void Test_BuscarAlumnoPorLegajo(){
+  Nodo *Lista = NULL;
+  Alumno a;
+strcpy(a.nombre, "Carlos3");
+a.edad = 20;
+a.Legajo = 150;
+Lista = agregar(copiar_estudiante(a), tipo_Alumno, Lista);
 
+Nodo *Busc = BuscarPorLegajo(Lista, 150);
+
+assert(Busc != NULL);
+Alumno *dato = (Alumno*)Busc->dato;
+assert(dato->Legajo == 150);
+assert(Lista != NULL);
+printf("Test Buscar Por Legajo Pasado\n");
+LiberarEspacioLista(Lista);
+}
+
+void Test_BuscarAlumnoPorLegajoError(){
+    Nodo *Lista = NULL;
+  Alumno a;
+strcpy(a.nombre, "Carlos4");
+a.edad = 20;
+a.Legajo = 90;
+Lista = agregar(copiar_estudiante(a), tipo_Alumno, Lista);
+
+Nodo *NoBusc = BuscarPorLegajo(Lista, 150);
+
+assert(NoBusc == NULL);
+printf("Test Buscar Por Legajo Erroneo Pasado\n");
+LiberarEspacioLista(Lista);
+}
 
 int main(){
 printf("Empezando Los Test\n");
 Test_AgregarAlumno();
 Test_AgregarAlumno_EdadErronea();
+Test_BuscarAlumnoPorLegajo();
+Test_BuscarAlumnoPorLegajoError();
 
-printf("Todos los Test Pasados Correctamente");
+printf("\nTodos los Test Pasados Correctamente");
 return 0;
 }
